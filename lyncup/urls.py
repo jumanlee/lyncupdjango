ad93329ee6a18b21921e157d6379a88f963dd371
Juman <jumanlee@live.com.au>
2025-02-27 01:23:06 +0000

    implemented unlike functionality in views.py

--- lyncup/urls.py
+++ lyncup/urls.py
@@ -37,7 +37,7 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-    path('users/', include('users.urls')),
+    path('api/users/', include('users.urls')),
     path('chat/', include('chat.urls')),
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-
-
 from rest_framework.views import APIView
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
@@ -13,8 +11,7 @@ from django.shortcuts import get_object_or_404
 from django.utils.timezone import now
 from django.contrib.auth import get_user_model 
 from django.core.exceptions import ValidationError
-
-
+from django.http import Http404
 from .models import *
 from .serializers import *
 
@@ -67,6 +64,48 @@ class LikeView(APIView):
             "like_count": like_instance.like_count,
             "last_like_date": like_instance.last_like_date
         },status=status.HTTP_200_OK)
+
+#this is mainly to reverse a like that has been made during the same chat session when a like was made.
+class UnlikeView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        #validate data, can share serializer with LikeView as there's no change in format
+        serializer = LikeSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        custom_user_model = get_user_model()
+
+        user_to_id = request.data.get("user_to")
+        user_from_instance = request.user
+        user_to_instance = get_object_or_404(custom_user_model, id=user_to_id)
+
+        if user_from_instance == user_to_instance:
+            raise ValidationError("User cannot unlike themself")
+
+        like_instance = get_object_or_404(Like, user_from=user_from_instance, user_to=user_to_instance)
+
+        if like_instance.like_count > 0:
+            like_instance.like_count -= 1
+            like_instance.save()
+
+        #return to the React side
+        return Response({
+            "user_from": user_from_instance.id,
+            "user_to": user_to_instance.id,
+            "like_count": like_instance.like_count,
+            "last_like_date": like_instance.last_like_date
+        },status=status.HTTP_200_OK)
+
+
+
+        
+
+        
+
+
+
 
 
 
