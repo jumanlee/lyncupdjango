f4b1fd4d8e91f72b1b20b6410f93d2d4515397b1
Juman <jumanlee@live.com.au>
2025-05-12 23:35:26 +0100

    minor fixes to consumers and views

--- direct_message/routing.py
+++ direct_message/routing.py
@@ -7,11 +7,11 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 
 #this is the first point of contact as this is in the asgi file, which routes to the consumer
 websocket_urlpatterns = [
-    re_path(r'ws/directmessage/(?P<conversation>\w+)/$', consumers.GroupConsumer.as_asgi())
+    re_path(r'ws/directmessage/(?P<conversation>\w+)/$', consumers.DirectMessageConsumer.as_asgi())
 ]
 
 #this is necessary as we have JWT token in the consumers connect method, what this does is it routes WebSocket connections without additional middleware for authentication.
-#application is defined in the asgi.py file, it knows this due to consumers.GroupConsumer.as_asgi() defined in websocket_urlpatterns.
+#application is defined in the asgi.py file, it knows this due to consumers.DirectMessageConsumer.as_asgi() defined in websocket_urlpatterns.
 
 # application = ProtocolTypeRouter({
 #     'websocket': URLRouter(websocket_urlpatterns)
