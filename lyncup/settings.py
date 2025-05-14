99918c650ad62f889bec33b447e2039bd5d319f9
Juman <jumanlee@live.com.au>
2025-05-14 13:04:39 +0100

    minor refactor in settings.py

--- lyncup/settings.py
+++ lyncup/settings.py
@@ -285,10 +285,10 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 # EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 EMAIL_HOST = config("EMAIL_HOST", default="smtp.gmail.com")
-EMAIL_PORT = 587
+EMAIL_PORT = config("EMAIL_PORT", default=587, cast=int)
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
 
 #base URL where React frontend handles email verification
 #It forms the beginning of the link the user clicks to confirm their email, like this:
